cbb39fca65b0a1d6f8f90f43fd75933b40db7c28
Rafael Souza <rafaelrs57@hotmail.com>
2020-01-09 14:51:41 -0300

    Finished Ch 06

---
+++ Ch_06/shapes.py
@@ -0,0 +1,19 @@
+'''
+Example of using matplotlib's Circle patch
+'''
+import matplotlib.pyplot as plt
+import matplotlib.animation as anim
+
+def create_circle():
+	circle = plt.Circle((0, 0), radius = 0.5)
+	return circle
+
+def show_shape(patch):
+	ax = plt.gca()
+	ax.add_patch(patch)
+	plt.axis('scaled')
+	plt.show()
+
+if __name__ == '__main__':
+	c = create_circle()
+	show_shape(c)
